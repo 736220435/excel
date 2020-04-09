9216b4e1292bd46d819ff797bbfc529132e2126e
Jonsson <yz909@outlook.com>
2020-04-09 23:58:50 +0800

    前端

--- src/main/java/com/springboot/controller/ComputerController.java
+++ src/main/java/com/springboot/controller/ComputerController.java
@@ -3,26 +3,28 @@ package com.springboot.controller;
 import com.github.pagehelper.PageInfo;
 import com.springboot.entity.Computer;
 import com.springboot.servie.ComputerService;
-import com.springboot.util.ResultVOUtils;
-import com.springboot.vo.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class ComputerController {
     @Autowired
     private ComputerService computerService;
 
     @RequestMapping("/computerList")
-    public ResultVO<Object> computerList(@RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum, @RequestParam(value = "pageSize", required = false, defaultValue = "5") Integer pageSize) {
+    public ModelAndView computerList(@RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum, @RequestParam(value = "pageSize", required = false, defaultValue = "5") Integer pageSize) {
+        ModelAndView modelAndView = new ModelAndView("computer");
         PageInfo<List<Computer>> listPageInfo = computerService.computerList(pageNum, pageSize);
         if (listPageInfo.getList().size() > 0) {
-            return ResultVOUtils.success(listPageInfo);
+            modelAndView.addObject("listPageInfo", listPageInfo);
+            return modelAndView;
         }
-        return ResultVOUtils.error("查询失败");
+        modelAndView.addObject("msg", "查询失败！");
+        return modelAndView;
     }
 }
