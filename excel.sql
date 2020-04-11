8e658668ca872240d54812f73b90b04dda212fdc
Jonsson <yz909@outlook.com>
2020-04-11 16:33:39 +0800

    数据库文件

--- excel.sql
+++ excel.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.29, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
 --
 -- Host: 127.0.0.1    Database: excel
 -- ------------------------------------------------------
--- Server version	5.7.29
+-- Server version	5.5.62
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,7 +29,7 @@ CREATE TABLE `car` (
   `colour` varchar(255) DEFAULT NULL COMMENT '颜色',
   `brand` varchar(255) DEFAULT NULL COMMENT '品牌',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='汽车';
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='汽车';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,61 +38,8 @@ CREATE TABLE `car` (
 
 LOCK TABLES `car` WRITE;
 /*!40000 ALTER TABLE `car` DISABLE KEYS */;
+INSERT INTO `car` VALUES (1,'东风',150,'黑色','东风'),(2,'丰田',100,'白色','丰田'),(3,'本田',120,'蓝色','本田'),(4,'东风',150,'黑色','东风'),(5,'丰田',100,'白色','丰田'),(6,'本田',120,'蓝色','本田'),(7,'东风',150,'黑色','东风');
 /*!40000 ALTER TABLE `car` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `computer`
---
-
-DROP TABLE IF EXISTS `computer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `computer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(255) DEFAULT NULL COMMENT '名称',
-  `price` int(11) DEFAULT NULL COMMENT '价格',
-  `cpu` varchar(255) DEFAULT NULL COMMENT 'cpu',
-  `disk` varchar(255) DEFAULT NULL COMMENT '硬盘',
-  `ram` varchar(255) DEFAULT NULL COMMENT '内存',
-  `system` varchar(255) DEFAULT NULL COMMENT '系统类型',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='电脑';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `computer`
---
-
-LOCK TABLES `computer` WRITE;
-/*!40000 ALTER TABLE `computer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `computer` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `student`
---
-
-DROP TABLE IF EXISTS `student`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `student` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(255) DEFAULT NULL COMMENT '姓名',
-  `age` int(11) DEFAULT NULL COMMENT '年龄',
-  `gender` int(11) DEFAULT NULL COMMENT '性别',
-  `grade` varchar(255) DEFAULT NULL COMMENT '班级',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='学生';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `student`
---
-
-LOCK TABLES `student` WRITE;
-/*!40000 ALTER TABLE `student` DISABLE KEYS */;
-/*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -104,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-09 11:23:11
+-- Dump completed on 2020-04-11 16:32:02
